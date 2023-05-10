ae9488573847f19e35ae6bed0ef1da9265000288
Bastien Silhol <bastien.silhol@epitech.eu>
2023-05-11 00:15:11 +0200

    feat: updated scripts

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -7,11 +7,13 @@ async function main() {
 
     const processor = process.env.PROCESSOR
     const trusted = process.env.TRUSTED
+    const tokenid = process.env.TOKENID
 
     const name = "TSTNM"
     const symbol = "TSTSMBL"
     if (!processor || !isAddress(processor)) throw new Error("Run command with PROCESSOR=0x env variable");
     if (!trusted || !isAddress(trusted)) throw new Error("Run command with TRUSTED=0x env variable");
+    if (!tokenid || typeof tokenid !== 'string') throw new Error("Run command with TOKENID=10002 env variable");
 
 
     const cbt = await ethers.getContractFactory("CoinBridgeToken"); 
@@ -26,10 +28,14 @@ async function main() {
     await proxy.deployed();
 
     const addSupplier = cbt.interface.encodeFunctionData('addSupplier', [tokenOwner.address]);
+    const setRules = cbt.interface.encodeFunctionData('setRules', [["11"], [tokenid]]);
+    const addAdministrator = cbt.interface.encodeFunctionData('addAdministrator', [tokenOwner.address]);
     const mint = cbt.interface.encodeFunctionData('mint', [tokenOwner.address, ONE_ETHER.mul("10000")]);
 
     const tx1 = await (proxy).connect(tokenOwner).fallback({ data: addSupplier })
-    const tx2 = await (proxy).connect(tokenOwner).fallback({ data: mint })
+    const tx2 = await (proxy).connect(tokenOwner).fallback({ data: addAdministrator })
+    const tx3 = await (proxy).connect(tokenOwner).fallback({ data: setRules })
+    const tx4 = await (proxy).connect(tokenOwner).fallback({ data: mint })
 
 
     console.log('Token created @', proxy.address);
