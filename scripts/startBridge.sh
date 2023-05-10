325458f2d46c2963997f9e91cb731979cb352611
Bastien Silhol <bastien.silhol@epitech.eu>
2023-05-10 18:30:44 +0200

    feat: updated startbridge script

--- scripts/startBridge.sh
+++ scripts/startBridge.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-yarn hardhat run scripts/deployArchitecture.ts --network home & pids=$!
-yarn hardhat run scripts/deployArchitecture.ts --network foreign & pids+=" $!"
+yarn hardhat run scripts/deployArchitecture.ts --network home &
+yarn hardhat run scripts/deployArchitecture.ts --network foreign &
 
-wait $pids
+wait
 
-PROCESSOR=0x2fc631e4B3018258759C52AF169200213e84ABab TRUSTED=0xF3F671199bF5960a053B875518F7C86D421aab55 yarn hardhat run scripts/deployToken.ts --network home & pids=$!
-PROCESSOR=0x2fc631e4B3018258759C52AF169200213e84ABab TRUSTED=0xF3F671199bF5960a053B875518F7C86D421aab55 yarn hardhat run scripts/deployToken.ts --network foreign & pids+=" $!"
+PROCESSOR=0x2fc631e4B3018258759C52AF169200213e84ABab TRUSTED=0xF3F671199bF5960a053B875518F7C86D421aab55 yarn hardhat run scripts/deployToken.ts --network home &
+PROCESSOR=0x2fc631e4B3018258759C52AF169200213e84ABab TRUSTED=0xF3F671199bF5960a053B875518F7C86D421aab55 yarn hardhat run scripts/deployToken.ts --network foreign &
 
-trap "kill $pids" SIGTERM SIGINT
-wait $pids
+wait
+
+echo "bridge-v2 done"
